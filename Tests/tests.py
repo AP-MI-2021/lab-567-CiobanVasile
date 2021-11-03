5c7f940903da61e07a5d191253be53b9149064a6
Vasi <v.cioban@yahoo.com>
2021-11-03 10:57:42 +0200

    ...

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,14 +1,30 @@
-from Domain.rezervare import creeaza_rezervare, get_id
+from Domain.rezervare import creeaza_rezervare, get_id, get_pret, get_clasa
 from Logic.crud import create, read, update, delete
+from Logic.ieftinire_rezervari_checking import ieftinire_procentaj
+from Logic.trecere_rezervari import trecere_rezervari_nume_clasa_superioara
 
 def get_data():
    return [
-    creeaza_rezervare(1, 'Vasile', 'economy', 12345, 'da'),
+    creeaza_rezervare(1, 'Vasile', 'economy', 100, 'da'),
     creeaza_rezervare(2, 'Adrian', 'bussines', 12000, 'nu'),
     creeaza_rezervare(3, 'Marton', 'bussines', 145, 'nu'),
     creeaza_rezervare(4, 'Pap', 'economy plus', 1235, 'da'),
     creeaza_rezervare(5, 'Halas', 'economy', 12, 'nu')
    ]
+
+def test_trecere_rezervari_nume_clasa_superioara():
+    rezervari = get_data()
+    trecere_rezervari_nume_clasa_superioara(rezervari, 'Alandala')
+
+    assert get_clasa(rezervari[0]) == 'economy'
+
+
+def test_ieftinire_procentaj():
+    rezervari = get_data()
+    pret_rez = get_pret(rezervari[0])
+    ieftinire_procentaj(rezervari, 0)
+
+    assert pret_rez == get_pret(rezervari[0])
 
 def test_create():
     rezervari = get_data()
@@ -47,6 +63,10 @@ def test_delete():
     r_to_delete = 2
     r_deleted = read(rezervari, r_to_delete)
     deleted = delete(rezervari, r_to_delete)
+
+def tests_functionalitati():
+    test_ieftinire_procentaj()
+    test_trecere_rezervari_nume_clasa_superioara()
 
 def test_crud():
     test_create()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Logic.crud import create
 from UserInterface.console import run_ui
-from Tests.tests import test_crud
+from Tests.tests import test_crud, tests_functionalitati
 
 
 def main():
@@ -9,4 +9,5 @@ def main():
 
 if __name__ == '__main__':
     test_crud()
+    tests_functionalitati()
     main()
